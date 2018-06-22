02e9bb23562de015cf6bf5b39cff03689824151b
Ihapmustapha <ihapmustapha@ymail.com>
2018-06-22 21:28:51 +0200

    Adding Navigation System

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import LeftSidebar from './LeftSidebar/LeftSidebar';
 import RightSidebar from './RightSidebar/RightSidebar';
 import Box from './Primitives/Box';
 import Cylinder from './Primitives/Cylinder';
-import lookControl from './ControlScene/lookControls';
-import EntitiesList from './EntitiesList';
+//import lookControl from './ControlScene/lookControls';
+//import EntitiesList from './EntitiesList';
 import './App.css';
 require('aframe-extras');
 require('aframe-gradient-sky');
+require('aframe-orbit-controls-component-2');
 
 
 
@@ -34,14 +35,32 @@ class App extends Component {
         })
     }
 
-
+    
 
     render() {
         return (
             <div>
                 <Scene>
-                    <lookControl />
+                {/*Basic Navigation Controls*/}
+                <a-entity id="rig"
+                        movement-controls
+                        position="25 0 25">
+                    <a-entity camera
+                        position="0 1.6 0"
+                        look-controls="pointerLockEnabled: true"></a-entity>
+                </a-entity>
 
+                {/*Scene Sky - Gradient Sky*/}
+                <a-gradient-sky
+                     material="shader: gradient; topColor: 255 255 255; bottomColor: 151 239 255;"
+                />
+
+
+                <a-entity 
+                scale="2 2 2"
+                rotation= "0 0 0"
+                >
+                
                     {/*User Interface Divs*/}
                     <LeftSidebar onButtonClicked={this.handleObjGen}/>
                     <RightSidebar />
@@ -50,13 +69,13 @@ class App extends Component {
                     {/*Default Grid*/}
                     <a-grid />
 
-                    {/*Scene Sky - Gradient Sky*/}
-                    <a-gradient-sky
-                        material="shader: gradient; topColor: 255 255 255; bottomColor: 151 239 255;"
-                    />
-
+                    {/*Scene Entities*/}
                     {this.state.entities}
-                    <EntitiesList />
+
+                    {/*Basic 3d Model For Testing loader*/}
+                    <a-entity scale="0.5 0.5 0.5" object-model="src: url(./3dModels/Bigmax_White_OBJ.obj);">
+                    </a-entity>
+                </a-entity> 
                 </Scene>
             </div>
         );
