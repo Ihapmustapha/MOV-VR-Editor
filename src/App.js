1e9f15820f29bd401d9b0e6498ac514cb8e65b70
Omar Bakier <omarbakeer@gmail.com>
2018-07-23 19:26:41 +0200

    refactor the render of Poly models + import mtl along with model-obj

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import 'aframe';
-import {Entity, Scene} from 'aframe-react';
-import 'aframe-particle-system-component';
+import { Entity, Scene } from 'aframe-react';
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import LeftSidebar from './LeftSidebar/LeftSidebar';
 import RightSidebar from './RightSidebar/RightSidebar';
-// import lookControl from './ControlScene/lookControls';
 import uuid from 'uuid';
 import './App.css';
 require('aframe-extras');
 require('aframe-gradient-sky');
 
 
-// const myColor = ['#D92B6A', '#9564F2', '#FFCF59']
-
-
 class App extends Component {
 
   state = {
+    // Default box that appears at the beginning of the scene
     colorIndex: 0,
     entities: [
       {
         id: uuid(),
         primitive: 'box',
         color: '#9013FE',
-        position: {x: 2, y: 3, z: 4},
-        rotation: {x: 2, y: 3, z: 4},
-        scale: {x: 2, y: 3, z: 4}
+        position: { x: 2, y: 3, z: 4 },
+        rotation: { x: 2, y: 3, z: 4 },
+        scale: { x: 2, y: 3, z: 4 }
       }
     ],
     selectedEntity: undefined,
     assets: []
   };
 
+  // Add new model/primitive to the state
   handleObjGen = (newObj) => {
     this.setState((prevState) => {
       return {
@@ -41,20 +37,16 @@ class App extends Component {
     })
   };
 
+  // Remove the selected model
   handleObjRem = (selectedEntitiy) => {
-    console.log("remove me");
-    this.setState((prevState) => {
-      return {
-        entities: prevState.entities.filter((entity) => {
-          if (entity.id !== selectedEntitiy) {
-            return entity;
-          }
-        })
-      };
+    this.setState((prevState) => ({
+      entities: prevState.entities.filter((entity) => entity.id !== selectedEntitiy)
     })
+    )
   };
 
-  handleClick = (e) => {
+  // Select a specific model/primitive with raycaster
+  handleSelect = (e) => {
     const selected = e.target.id;
     this.setState({
       selectedEntity: selected
@@ -70,11 +62,11 @@ class App extends Component {
         entities: prevState.entities.map((entity) => {
           if (entity.id === prevState.selectedEntity) {
             if (axisVal === 'xPosition') {
-              return {...entity, position: {...entity.position, x: newVal} }
-            }else if (axisVal === 'yPosition') {
-              return {...entity, position: {...entity.position, y: newVal} }
-            }else if (axisVal === 'zPosition') {
-              return {...entity, position: {...entity.position, z: newVal} }
+              return { ...entity, position: { ...entity.position, x: newVal } }
+            } else if (axisVal === 'yPosition') {
+              return { ...entity, position: { ...entity.position, y: newVal } }
+            } else if (axisVal === 'zPosition') {
+              return { ...entity, position: { ...entity.position, z: newVal } }
             }
           }
           return entity;
@@ -91,11 +83,11 @@ class App extends Component {
         entities: prevState.entities.map((entity) => {
           if (entity.id === prevState.selectedEntity) {
             if (axisVal === 'xRotation') {
-              return {...entity, rotation: {...entity.rotation, x: newVal} }
-            }else if (axisVal === 'yRotation') {
-              return {...entity, rotation: {...entity.rotation, y: newVal} }
-            }else if (axisVal === 'zRotation') {
-              return {...entity, rotation: {...entity.rotation, z: newVal} }
+              return { ...entity, rotation: { ...entity.rotation, x: newVal } }
+            } else if (axisVal === 'yRotation') {
+              return { ...entity, rotation: { ...entity.rotation, y: newVal } }
+            } else if (axisVal === 'zRotation') {
+              return { ...entity, rotation: { ...entity.rotation, z: newVal } }
             }
           }
           return entity;
@@ -112,11 +104,11 @@ class App extends Component {
         entities: prevState.entities.map((entity) => {
           if (entity.id === prevState.selectedEntity) {
             if (axisVal === 'xScale') {
-              return {...entity, scale: {...entity.scale, x: newVal} }
-            }else if (axisVal === 'yScale') {
-              return {...entity, scale: {...entity.scale, y: newVal} }
-            }else if (axisVal === 'zScale') {
-              return {...entity, scale: {...entity.scale, z: newVal} }
+              return { ...entity, scale: { ...entity.scale, x: newVal } }
+            } else if (axisVal === 'yScale') {
+              return { ...entity, scale: { ...entity.scale, y: newVal } }
+            } else if (axisVal === 'zScale') {
+              return { ...entity, scale: { ...entity.scale, z: newVal } }
             }
           }
           return entity;
@@ -125,12 +117,13 @@ class App extends Component {
     })
   };
 
+  // Change color of the selected model/primitive
   handleColorChange = (color) => {
     this.setState((prevState) => {
       return {
         entities: prevState.entities.map((entity) => {
           if (entity.id === prevState.selectedEntity) {
-            return {...entity, color: color.hex }
+            return { ...entity, color: color.hex }
           }
           return entity;
         })
@@ -139,46 +132,48 @@ class App extends Component {
   }
 
   onRequestHandler = (response) => {
-    this.onResults(response,this.importAsset);
+    this.onResults(response, this.importAsset);
   };
 
-  importAsset = (asset) => {
-    console.log(asset);
+  importAsset = (assetOBJ, assetMTL) => {
+    console.log(assetOBJ);
     this.setState((prevState) => {
       return {
         assets: prevState.assets.concat({
-          id: asset,
-          position: {x: 0, y:  0, z: 0},
+          id: uuid(),
+          obj: assetOBJ,
+          mtl: assetMTL,
+          position: { x: 0, y: 0, z: 0 },
           color: '#9013FE',
-          rotation: {x: 0, y: 0, z: 0},
-          scale: {x: 2, y: 2, z: 2}
+          rotation: { x: 0, y: 0, z: 0 },
+          scale: { x: 2, y: 2, z: 2 }
         })
       }
     })
+    console.log(this.state.assets)
   };
 
   onResults = (data, callback) => {
     console.log(data);
-    while ( document.getElementById('SearchResults').childNodes.length ) {
-      document.getElementById('SearchResults').removeChild( document.getElementById('SearchResults').firstChild );
-    }
+    document.getElementById('SearchResults').innerHTML = ''
+    // while (document.getElementById('SearchResults').childNodes.length) {
+    //   document.getElementById('SearchResults').removeChild(document.getElementById('SearchResults').firstChild);
+    // }
 
     const assets = data.assets;
-    if ( assets ) {
-      for ( let i = 0; i < assets.length; i ++ ) {
-        let asset = assets[ i ];
-        //var image = createImage( asset );
-
+    if (assets) {
+      for (let i = 0; i < assets.length; i++) {
+        let asset = assets[i];
         const image = document.createElement('img');
         image.src = asset.thumbnail.url;
         image.style.width = '40px';
         image.style.height = '30px';
         image.addEventListener('click', function (event) {
-          callback(asset.formats[0].root.url);
+          callback(asset.formats[0].root.url, asset.formats[0].resources[0].url);
         });
 
         // ReactDOM.render(<Image/>, document.getElementById('SearchResults'));
-        document.getElementById('SearchResults').append( image );
+        document.getElementById('SearchResults').append(image);
       }
     }
   };
@@ -186,40 +181,49 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <LeftSidebar
-            onButtonClicked={this.handleObjGen}
-            onRemoveClicked={this.handleObjRem}
-            onRequestHandler={this.onRequestHandler}
-            selectedEntitiy={this.state.entities.filter((entity) => {
-              if (entity.id === this.state.selectedEntity){
-                return entity;
-              }
-            })[0]}
-          />
-        </div>
+
+        {/*User Interface Divs*/}
+
+        <RightSidebar
+          onPositionChange={this.handlePositionChange}
+          onRotationChange={this.handleRotationChange}
+          onScaleChange={this.handleScaleChange}
+          onColorChange={this.handleColorChange}
+          // [TODO: change this callback into a separated function]
+          selectedEntitiy={this.state.entities.filter((entity) => {
+            if (entity.id === this.state.selectedEntity) {
+              return entity;
+            }
+          })[0]}
+        />
+
+        <LeftSidebar
+          onButtonClicked={this.handleObjGen}
+          onRemoveClicked={this.handleObjRem}
+          onRequestHandler={this.onRequestHandler}
+          // [TODO: change this callback into a separated function]
+          selectedEntitiy={this.state.entities.filter((entity) => {
+            if (entity.id === this.state.selectedEntity) {
+              return entity;
+            }
+          })[0]}
+        />
+
         <Scene>
 
-          {/*User Interface Divs*/}
-
-          <RightSidebar
-            onPositionChange={this.handlePositionChange}
-            onRotationChange={this.handleRotationChange}
-            onScaleChange={this.handleScaleChange}
-            onColorChange={this.handleColorChange}
-            selectedEntitiy={this.state.entities.filter((entity) => {
-              if (entity.id === this.state.selectedEntity){
-                return entity;
-              }
-            })[0]}
+          {/*Default Grid*/}
+          <Entity
+            primitive="a-grid"
           />
 
-          {/*Default Grid*/}
-          <a-grid />
-
           {/*Scene Sky - Gradient Sky*/}
-          <a-gradient-sky
-            material="shader: gradient; topColor: 255 255 255; bottomColor: 151 239 255;"
+          <Entity
+            primitive="a-sky"
+            material={{
+              shader: 'gradient',
+              topColor: { x: 255, y: 255, z: 255 },
+              bottomColor: { x: 151, y: 239, z: 255 }
+            }}
           />
 
           <Entity primitive="a-camera" look-controls >
@@ -237,44 +241,44 @@ class App extends Component {
               <Entity class="clickable"
                 id={entity.id}
                 key={entity.id}
-                geometry={{primitive: entity.primitive}}
-                material={{color: entity.color}}
+                geometry={{ primitive: entity.primitive }}
+                material={{ color: entity.color }}
                 position={entity.position}
                 rotation={entity.rotation}
                 scale={entity.scale}
-                events={{click: this.handleClick}}
+                events={{ click: this.handleSelect }}
               />
             );
           })}
 
-
-
-          {this.state.assets.map((asset) => {
-            return (
-              <a-asset-item
-                id={asset.id}
-                key={asset.id}
-                src={asset.id}>
-              </a-asset-item>
-              );
-          })}
+          {/* Imported models from the poly API */}
 
           {this.state.assets.map((asset) => {
             return (
               <Entity class="clickable"
                 id={asset.id}
                 key={asset.id}
-                // material={{color: 'red'}}
-                obj-model={`obj: #${asset.id}`}
-                material={{color: asset.color}}
+                obj-model={`obj: #${asset.obj}; mtl: #${asset.mtl}`}
                 position={asset.position}
                 rotation={asset.rotation}
                 scale={asset.scale}
-                events={{click: this.handleClick}}
-              />
-              );
+                events={{ click: this.handleSelect }}
+              >
+                <Entity
+                  primitive="a-asset-item"
+                  id={asset.obj}
+                  key={asset.obj}
+                  src={asset.obj}
+                />
+                <Entity
+                  primitive="a-asset-item"
+                  id={asset.mtl}
+                  key={asset.mtl}
+                  src={asset.mtl}
+                />
+              </Entity>
+            );
           })}
-
 
         </Scene>
       </div>
@@ -282,4 +286,4 @@ class App extends Component {
   }
 }
 
-  export default App;
+export default App;
